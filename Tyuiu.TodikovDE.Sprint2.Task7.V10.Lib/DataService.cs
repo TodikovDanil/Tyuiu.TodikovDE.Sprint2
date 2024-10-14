25ef138cdd57aabbd991e4fdbd5834331334cd18
TodikovDanil <todic.danil@gmail.com>
2024-10-14 17:18:14 +0300

    Task7

---
+++ Tyuiu.TodikovDE.Sprint2.Task7.V10.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.TodikovDE.Sprint2.Task7.V10.Lib
+{
+    public class DataService : ISprint2Task7V10
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y >= Math.Pow(x, 2) - 2) && (x <= 2) && (x >= -2) && (y >= -2) && (y <= 2))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.TodikovDE.Sprint2.Task7.V10/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.TodikovDE.Sprint2.Task7.V10.Lib;
+namespace Tyuiu.TodikovDE.Sprint2.Task7.V10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+
+
+            Console.Title = "Спринт #2 | Выполнил: Тодиков Д. Э. | ИСТНб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #2                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #8                                                             *");
+            Console.WriteLine("* Выполнил: Тодиков Даниил Эдуардович | ИСТНб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            
+            Console.WriteLine("Введите значение Х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованой области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованой области");
+            }
+            Console.ReadKey();
+        }
+    }
+}
